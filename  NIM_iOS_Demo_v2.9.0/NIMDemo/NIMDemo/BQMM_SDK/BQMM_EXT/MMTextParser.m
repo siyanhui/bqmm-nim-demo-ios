290f61576725f5df64d10347b3739fa94cc48998
isan <3isan333@gmail.com>
2016-11-22 14:18:15 +0800

    更新BQMM 1.7.7

---  NIM_iOS_Demo_v2.9.0/NIMDemo/NIMDemo/BQMM_SDK/BQMM_EXT/MMTextParser.m
+++  NIM_iOS_Demo_v2.9.0/NIMDemo/NIMDemo/BQMM_SDK/BQMM_EXT/MMTextParser.m
@@ -88,7 +88,7 @@
                                              options:NSStringDrawingUsesLineFragmentOrigin|NSStringDrawingUsesFontLeading
                                              context:nil].size;
     
-    return CGRectIntegral(CGRectMake(0, 0, sizeToFit.width + 10, sizeToFit.height)).size;
+    return CGRectIntegral(CGRectMake(0, 0, sizeToFit.width + 10, sizeToFit.height + 16)).size;
 }
 
 + (CGSize)sizeForTextWithText:(NSString *)text
@@ -97,6 +97,7 @@
     CGSize size = CGSizeZero;
     size = [text boundingRectWithSize:CGSizeMake(maximumTextWidth, CGFLOAT_MAX) options:NSStringDrawingUsesLineFragmentOrigin|NSStringDrawingUsesFontLeading attributes:@{NSFontAttributeName:font} context:nil].size;
     size.width += 10;
+    size.height += 16;
     
     return size;
 }
